63d7ae94303073de42d75eaf32c8f09bb8285823
reza72rg <reza72rg@gmail.com>
2024-08-06 09:12:03 +0330

    Create urls accounts.

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth.views import LogoutView
+
+
+# Set the app name for namespacing
+
+app_name = "accounts"
+
+
+urlpatterns = [
+
+]
